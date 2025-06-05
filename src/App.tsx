e987ea039d9550a2c8d636909e9ddb40502c9e17
Ioannis Matthioudakis <imatthioudakis@gmail.com>
2025-06-05 21:03:49 +0300

    feat: new examples page/section/routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,27 @@
 // import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 // import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
-import Layout from "./components/Layout.tsx";
+// import Layout from "./components/Layout.tsx";
 // import CounterAdvancedWithCustomHook from "./components/CounterAdvancedWithCustomHook.tsx";
 // import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
 // import CounterAdvanced from "./components/CounterAdvanced.tsx";
 // import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
 // import Counter from "./components/Counter.tsx";
-import NameChanger from "./components/NameChanger.tsx";
+// import NameChanger from "./components/NameChanger.tsx";
 // import {useEffect} from "react";
 // import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
 // import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 // import CounterWithReducer from "./components/CounterWithReducer";
-import OnlineStatus from "./components/OnlineStatus.tsx";
+// import OnlineStatus from "./components/OnlineStatus.tsx";
 // import Todo from "./components/Todo/Todo.tsx";
+import {BrowserRouter, Routes, Route} from "react-router";
+import HomePage from "./pages/HomePage.tsx";
+import NameChangerPage from "./pages/NameChangerPage.tsx";
+import OnlineStatusPage from "./pages/OnlineStatusPage.tsx";
+import UserPage from "./pages/UserPage.tsx";
+import RouterLayout from "./components/RouterLayout.tsx";
+import ExamplesPage from "./pages/ExamplesPage.tsx";
+import RouterExamplesLayout from "./components/RouterExamplesLayout.tsx";
 
 function App() {
 
@@ -28,7 +36,7 @@ function App() {
 
   return (
     <>
-        <Layout>
+        {/*<Layout>*/}
       {/*<ViteIntro />*/}
       {/*  <ClassComponent />*/}
       {/*  <FunctionalComponent />*/}
@@ -43,7 +51,7 @@ function App() {
             {/*<h1 className="text-center etxt-2xl font-bold">This is a heading title</h1>*/}
             {/*<FunctionalComponent />*/}
             {/*<Counter/>*/}
-            <NameChanger/>
+            {/*<NameChanger/>*/}
             {/*<CounterWithMoreStates/>*/}
             {/*<CounterAdvanced/>*/}
             {/*<CounterWithCustomHook/>*/}
@@ -51,8 +59,27 @@ function App() {
             {/*<CounterWithReducer />*/}
             {/*<Todo/>*/}
 
-            <OnlineStatus/>
-        </Layout>
+            {/*<OnlineStatus/>*/}
+        {/*</Layout>*/}
+
+        <BrowserRouter>
+            {/*<Layout>*/}
+                <Routes>
+                    {/*<Route path="/" element={<HomePage />} />*/}
+                    <Route element={<RouterLayout />}>
+                        <Route index element={<HomePage />} />
+                    </Route>
+                    {/*<Route path="examples?" element={<ExamplesPage />}>*/}
+                    <Route path="examples" element={<RouterExamplesLayout/>}>
+                        <Route index element={<ExamplesPage />}/>
+                        <Route path="name-changer" element={<NameChangerPage />} />
+                        <Route path="online-status" element={<OnlineStatusPage />} />
+                    </Route>
+                    <Route path="users/:userId" element={<UserPage />}/>
+                    {/*<Route path="files/*" element={<FilePage />}/>*/}
+                </Routes>
+            {/*</Layout>*/}
+        </BrowserRouter>
 
     </>
   )
