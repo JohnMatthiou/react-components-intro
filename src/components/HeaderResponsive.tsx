58a0d42d8b8d2c43a641285ef8a2b5d892394bac
Ioannis Matthioudakis <imatthioudakis@gmail.com>
2025-07-12 18:30:02 +0300

    style: add z-50 to header element

--- src/components/HeaderResponsive.tsx
+++ src/components/HeaderResponsive.tsx
@@ -8,7 +8,7 @@ const HeaderResponsive = () => {
 
     return (
         <>
-            <header className="bg-[#782024] fixed w-full">
+            <header className="bg-[#782024] z-50 fixed w-full">
                 <div className="container mx-auto px-4 flex items-center justify-between">
                     <CodingFactoryLogo />
 
